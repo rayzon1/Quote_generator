c011af493d734b3447c0a3f5d9a0507a4a456402
Gerardo Keys <jotojry@icloud.com>
2019-02-18 22:14:18 -0800

    02/18/19 final refactor with es6 standard

--- js/quote_logic.js
+++ js/quote_logic.js
@@ -2,40 +2,40 @@
 let message = '';
 
 // function will print the text to the html page
-function printQuote(text) {
+const printQuote = (text) => {
     let outputDiv = document.getElementById("output");
     outputDiv.innerHTML = text;
   }
 
 // function will get a random quote, store it in string message then return the message
-function getRandomQuote() {
+const getRandomQuote = () => {
     let random = Math.floor(Math.random() * quotes.length);
     let quoteSection = quotes[random];
-    message = '<h1 id="quote1">' + quoteSection['quote'] + '</h1>';
-    message += '<h2 id="quote2">' + quoteSection['source'] + '</h1>';
+    message = `<h1 id="quote1"> ${quoteSection['quote']} </h1>`;
+    message += `<h2 id="quote2"> ${quoteSection['source']} </h1>`;
     return message;
 }
 
 // function will return a random number between 1 - 255
-function getRandomColor() {
+const getRandomColor = () => {
     let random = Math.floor(Math.random() * 255);
     return random;
 }
 
 // function will apply color and background styles using rgb color values
-function printRandomColor(a, b, c) {
-    document.getElementById("button").style.background = "rgba(" +  a + "," + b + "," + c + ", 0.4)";
-    document.getElementById("quote1").style.color = "rgb(" +  a + "," + b + "," + c + ")";
-    document.getElementById("quote2").style.color = "rgb(" +  a + "," + b + "," + c + ")";
+const printRandomColor = (a, b, c) => {
+    document.getElementById("button").style.background = `rgba(${a}, ${b}, ${c}, 0.4)`;
+    document.getElementById("quote1").style.color = `rgb(${a}, ${b}, ${c})`;
+    document.getElementById("quote2").style.color = `rgb(${a}, ${b}, ${c})`;
 }
 
 // this function scales back the transform property in CSS
-function buttonConfig() {
+const buttonConfig = () => {
     document.getElementById("button").style.transform = "scale(1.0) rotate(360deg)";
 }
 
 // main function will set color variables to return value of getRandomColor() function
-function main() {
+const main = () => {
     color = getRandomColor();
     color1 = getRandomColor();
     color2 = getRandomColor();
